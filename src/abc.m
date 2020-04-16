8e0b6b35c64200bae32b37e06ce8da8e3ec5d316
matt <mdailis@ccs.neu.edu>
2020-04-16 17:58:12 -0400

    Added section about damping

--- src/abc.m
+++ src/abc.m
@@ -1,39 +1,46 @@
 disp("Running abc.m...");
 
-function y = make_decay1(x)
-  y = 0.5 ^ (x / 20000);
+function y = damping(x, dampingTime, bitRate)
+  y = 0.5 ^ (x / (dampingTime * bitRate));
 endfunction
 
-function y = make_decay2(x)
-  y = 0.5 ^ (x / 10000);
+function y = createDamping(bitRate, duration, dampingTime)
+  y = arrayfun(@(x) damping(x, bitRate, dampingTime), [1 : bitRate * duration]);
 endfunction
 
-function playSound(vector, bit_rate)
-  ## silence = zeros(1, bit_rate);
-  ## player = audioplayer ([silence, vector], bit_rate, 16);
-  player = audioplayer (vector, bit_rate, 16);
+function playSound(vector, bitRate)
+  player = audioplayer (vector, bitRate, 16);
   play (player);
   while(isplaying(player))
   endwhile
 endfunction
 
-function y = puretone(BIT_RATE, seconds, frequency, phase_shift=0)
-  y = sinewave(BIT_RATE * seconds, BIT_RATE/frequency, phase_shift);
+function y = puretone(bitRate, seconds, frequency, phaseShift=0)
+  y = sinewave(bitRate * seconds, bitRate/frequency, phaseShift);
 endfunction
 
-function y = createharmonics(BIT_RATE, DURATION, fundamental)
-  decay1 = arrayfun(@make_decay1, [1 : BIT_RATE * DURATION]);
-  decay2 = arrayfun(@make_decay2, [1 : BIT_RATE * DURATION]);
-  f1 = puretone(BIT_RATE, DURATION, fundamental); % .* decay1;
-  f2 = puretone(BIT_RATE, DURATION, fundamental * 2); % .* decay2;
-  f3 = puretone(BIT_RATE, DURATION, fundamental * 3); % .* decay2;
-  f4 = puretone(BIT_RATE, DURATION, fundamental * 4); % .* decay2;
-  f5 = puretone(BIT_RATE, DURATION, fundamental * 5); % .* decay2;
-  f6 = puretone(BIT_RATE, DURATION, fundamental * 6); % .* decay2;
-  y = f1; ## (f1 + f2 + f3 + f4 + f5 + f6) / 6;
+function y = createtone(bitRate, duration, frequency, dampingFactors)
+  y = puretone(bitRate, duration, frequency) .* dampingFactors;
 endfunction
 
-function m = maxfft(BIT_RATE, X, checkFreq)
+function y = createharmonics(bitRate, duration, fundamental, weights)
+
+  dampingFactors = createDamping(bitRate, duration, 0.3);
+
+  M = [];
+
+  ## Build up matrix where each row is another harmonic
+  for index = 1 : length(weights)
+    M = [M; weights(index) * createtone(bitRate, duration, fundamental * index, dampingFactors)];
+  endfor
+
+  ## Collapse them at the end
+  S = sum(M);
+  
+  y = S / max(S);
+endfunction
+
+function m = maxfft(bitRate, X, checkFreq)
   L = length(X);
   Y = fft(X);
   P2 = abs(Y / L);
@@ -42,7 +49,7 @@ function m = maxfft(BIT_RATE, X, checkFreq)
 
   [maxVal, maxIndex] = max(P1);
 
-  ## maxFreq = maxIndex * BIT_RATE / length(X);
+  ## maxFreq = maxIndex * bitRate / length(X);
 
   maxVal
   maxIndex
@@ -57,12 +64,12 @@ function m = maxfft(BIT_RATE, X, checkFreq)
   m = maxIndex; # maxFreq;
 endfunction
 
-function v = ffttest(BIT_RATE, X, checkFreq)
+function v = ffttest(bitRate, X, checkFreq)
   L = length(X);
   Y = fft(X);
   P2 = abs(Y / L);
   P1 = P2(1:(L / 2) + 1);
-  checkIndex = checkFreq * L / BIT_RATE;
+  checkIndex = checkFreq * L / bitRate;
   [checkVal, checkIndex2] = max(P1(checkIndex - 5 : checkIndex + 5));
   v = checkVal;
 endfunction
@@ -76,51 +83,66 @@ function y = kink(L, location)
   y = [y1(1:k*L), y2(k*L+1:L)];
 endfunction
 
+## Returns the first 10 harmonic weights for the given pluck location
+function y = getHarmonicWeights(pluckLocation)
+  v = kink(1000, pluckLocation);
+  X = abs(fft(v - mean(v)));
+  y = (X / max(X))(2:10);
+endfunction
+
+function filtertest()
+  sf = 800; sf2 = sf/2;
+  data=[[1;zeros(sf-1,1)],sinetone(25,sf,1,1),sinetone(50,sf,1,1),sinetone(100,sf,1,1)];
+  [b,a]=butter ( 1, 50 / sf2 );
+  filtered = filter(b,a,data);
+endfunction
+
 function main()
-  BIT_RATE = 44100;
-  A3 = createharmonics(BIT_RATE, 0.5, 220);
-  A4 = createharmonics(BIT_RATE, 0.5, 440);
-  C4 = createharmonics(BIT_RATE, 0.5, 523.26);
-  E4 = createharmonics(BIT_RATE, 0.5, 660);
-  E3 = createharmonics(BIT_RATE, 0.5, 330);
+  bitRate = 44100;
 
-  B4 = createharmonics(BIT_RATE, 0.5, 495);
-  D3 = createharmonics(BIT_RATE, 0.5, 293.33);
-  D4 = createharmonics(BIT_RATE, 0.5, 293.33 * 2);
-  GS4 = createharmonics(BIT_RATE, 0.5, 415.305);
+  weights = getHarmonicWeights(0.15);
+  duration = 2.0;
+  
+  A3 = createharmonics(bitRate, duration, 220, weights);
 
-  F5 = createharmonics(BIT_RATE, 0.5, 348.84 * 2);
-  D5 = createharmonics(BIT_RATE, 0.5, 293.33 * 4);
-  A5 = createharmonics(BIT_RATE, 0.5, 880);
+  ## A4 = createharmonics(bitRate, duration, 440, weights);
+  C3 = createharmonics(bitRate, duration, 523.26 / 2, weights);
+  ## C4 = createharmonics(bitRate, duration, 523.26, weights);
+  ## E4 = createharmonics(bitRate, duration, 660, weights);
+  E3 = createharmonics(bitRate, duration, 330, weights);
+  E2 = createharmonics(bitRate, duration, 330 / 2, weights);
 
-  aMinor = [A4, (C4 + E4) / 2, E3, (C4 + E4) / 2];
-  eMajor = [B4, (E4 + GS4) / 2, E3, (D4 + GS4) / 2];
-  dMinor = [A4, (D4 + F5) / 2, D3, (D4 + F5) / 2];
+  ## B4 = createharmonics(bitRate, duration, 495, weights);
+  ## D3 = createharmonics(bitRate, duration, 293.33, weights);
+  ## D4 = createharmonics(bitRate, duration, 293.33 * 2, weights);
+  ## GS4 = createharmonics(bitRate, duration, 415.305, weights);
 
-  ## v = ffttest(BIT_RATE, A3, 220)
+  ## F5 = createharmonics(bitRate, duration, 348.84 * 2, weights);
+  ## D5 = createharmonics(bitRate, duration, 293.33 * 4, weights);
+  ## A5 = createharmonics(bitRate, duration, 880, weights);
 
-  v = kink(100, 0.5);
-  abs(fft(v))(1:10)
+  aMinor = [A3, (C3 + E3) / 2, E2, (C3 + E3) / 2];
+  ## eMajor = [B4, (E4 + GS4) / 2, E3, (D4 + GS4) / 2];
+  ## dMinor = [A4, (D4 + F5) / 2, D3, (D4 + F5) / 2];
+
+  ## v = ffttest(bitRate, A3, 220)
 
   ## ffttest(100, v, 10)
 
   ## disp("A4");
-  ## ffttest(BIT_RATE, A4);
+  ## ffttest(bitRate, A4);
 
   ## disp("E3");
-  ## ffttest(BIT_RATE, E3);
+  ## ffttest(bitRate, E3);
 
   ## disp("E4");
-  ## ffttest(BIT_RATE, E4);
+  ## ffttest(bitRate, E4);
 
-  ## song = [aMinor, eMajor, aMinor, eMajor, dMinor, aMinor, eMajor, A4, E3, A3];
-  ## playSound(song, BIT_RATE)
-  ## audiowrite("output.wav", song, BIT_RATE);
-
-  v = kink(1000, 0.5); bar(abs(fft(v-mean(v)))(1:10)(2:end))
-
-  
+  song = [aMinor]; #, eMajor, aMinor, eMajor, dMinor, aMinor, eMajor, A4, E3, A3];
+  playSound(song, bitRate)
+  ## audiowrite("with_damping.wav", song, bitRate);
 endfunction
 
-main()
+##main()
+filtertest()
 disp("Finished abc.m");
